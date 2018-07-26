58fbef400d04728f8dd3f9a97b88b99e969b81e0
Hal Wang <halhaoranwang@gmail.com>
2018-07-26 20:01:35 +0800

    finished 24-game

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardOne: UILabel!
+    @IBOutlet weak var cardTwo: UILabel!
+    @IBOutlet weak var cardThree: UILabel!
+    @IBOutlet weak var cardFour: UILabel!
+
+    @IBAction func deal(_ sender: Any) {
+        let number1 = arc4random() % 10 + 1
+        cardOne.text = "\(number1)"
+        
+        let number2 = arc4random() % 10 + 1
+        cardTwo.text = "\(number2)"
+        
+        let number3 = arc4random() % 10 + 1
+        cardThree.text = "\(number3)"
+        
+        let number4 = arc4random() % 10 + 1
+        cardFour.text = "\(number4)"
+    }
     
-   
-    
+
 }
 
 
