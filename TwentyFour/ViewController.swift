15c91a9b6ad73538bac1303bebf4f2ea473e73a9
chandler <chandler@chandlerdeMBP.lan>
2018-07-26 12:01:23 +0800

    finished app 24

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardOne: UILabel!
+    @IBOutlet weak var cardTwo: UILabel!
+    @IBOutlet weak var cardThree: UILabel!
+    @IBOutlet weak var cardFour: UILabel!
+ 
+    @IBAction func deal(_ sender: Any) {
+        
+        let numberOne = arc4random() % 10 + 1
+        cardOne.text = "\(numberOne)"
+        
+        let numberTwo = arc4random() % 10 + 1
+        cardTwo.text = "\(numberTwo)"
     
-   
-    
+        let numberThree = arc4random() % 10 + 1
+        cardThree.text = "\(numberThree)"
+        
+        let numberFour = arc4random() % 10 + 1
+        cardFour.text = "\(numberFour)"
+        
+    }
 }
 
 
