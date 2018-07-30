80286bb45613e3b12f5a198dfd6f7a40b6dd5811
daobao <dabao@daobaodeMacBook-Air.local>
2018-07-31 07:15:03 +0800

    added app icon

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardOne: UILabel!
+    @IBOutlet weak var cardTwo: UILabel!
+    @IBOutlet weak var cardThree: UILabel!
+    @IBOutlet weak var cardFour: UILabel!
     
-   
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        let numberOne = arc4random() % 10 + 1
+        cardOne.text = "\(numberOne)"
+        let numberTwo = arc4random()  % 10 + 1
+        cardTwo.text = "\(numberTwo)"
+        let numberThree = arc4random()  % 10 + 1
+        cardThree.text = "\(numberThree)"
+        let numberFour = arc4random()  % 10 + 1
+        cardFour .text = "\(numberFour)"
+        
+        
+        
+        
+        
+        
+    }
+
     
 }
 
