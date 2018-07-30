f7011403efc257da4c93a6da743ed3a0bea6d686
Hanson Wen <Hanson@RossideMacBook-Pro.local>
2018-07-29 21:12:45 -0400

    migrated existing code to our new 24-ios project

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardOneLabel: UILabel!
+    @IBOutlet weak var cardTwoLabel: UILabel!
+    @IBOutlet weak var cardThreeLabel: UILabel!
+    @IBOutlet weak var cardFourLabel: UILabel!
     
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        print("anything")
+        let cardOneValue = arc4random() % 10
+        let cardTwoValue = arc4random() % 10
+        let cardThreeValue = arc4random() % 10
+        let cardFourValue = arc4random() % 10
+        cardOneLabel.text = "\(cardOneValue + 1)"
+        cardTwoLabel.text = "\(cardTwoValue + 1)"
+        cardThreeLabel.text = "\(cardThreeValue + 1)"
+        cardFourLabel.text = "\(cardFourValue + 1)"
+        
+    }
    
     
 }
