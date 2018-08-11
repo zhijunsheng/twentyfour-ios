02380d223b4b3bae16bc082bea5ad529d3d0bb0e
Sharon Chen <sharonchen@Sharons-MacBook-Air.local>
2018-08-11 11:29:25 -0400

    Created code for twentyfour and aligned labels and button using autolayout

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var card0Label: UILabel!
+    @IBOutlet weak var card1Label: UILabel!
+    @IBOutlet weak var card2Label: UILabel!
+    @IBOutlet weak var card3Label: UILabel!
     
-   
-    
+    @IBAction func touchNext(_ sender: UIButton) {
+        let card0Value = arc4random() % 10 + 1
+        let card1Value = arc4random() % 10 + 1
+        let card2Value = arc4random() % 10 + 1
+        let card3Value = arc4random() % 10 + 1
+        card0Label.text = "\(card0Value)"
+        card1Label.text = "\(card1Value)"
+        card2Label.text = "\(card2Value)"
+        card3Label.text = "\(card3Value)"
+        
+    }
 }
 
 
