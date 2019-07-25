783e5d993a6f3aa03ba22874df90fc4e800b5de8
Andy Yan <andy@Andys-MacBook-Air.local>
2019-07-24 20:01:58 -0400

    we finished our 24 game!

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,13 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var firstCardLabel: UILabel!
-    
+    @IBOutlet weak var secondCardLabel: UILabel!
+    @IBOutlet weak var thirdCardLabel: UILabel!
+    @IBOutlet weak var fourthCardLabel: UILabel!
+
     @IBAction func buttonTouched(_ sender: Any) {
-        print("hi guys")
-        firstCardLabel.text = "5"
+        let x = arc4random()
+        print("\(x)")
+        let smallerX = x % 10
+        print("\(smallerX)")
+        // what is the remainder of 19 ÷ 5 ?
+        // / %
+        // 19 % 5 is 4
+        // 95327 % 100 is 27
+        
+        firstCardLabel.text = "\(smallerX)"
+        
+        
+        let y = arc4random()
+        let smallerY = y % 10
+        secondCardLabel.text = "\(smallerY)"
+        
+        
+        let z = arc4random()
+        let smallerZ = z % 10
+        thirdCardLabel.text = "\(smallerZ)"
+        
+        
+        
+        let a = arc4random()
+        let smallerA = a % 10
+        fourthCardLabel.text = "\(smallerA)"
+        
+        
+        
     }
     
     
