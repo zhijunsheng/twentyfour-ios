296229923032c97a53de0405e95df1b6f113a951
Jocelyn Chen <jocelynchen@Jocelyns-MacBook.local>
2019-07-30 15:15:30 -0400

    we created four labels and a button, but there are some small issues with the random number generator

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var cardOne: UILabel!
+    @IBOutlet weak var cardTwo: UILabel!
+    @IBOutlet weak var cardThree: UILabel!
+    @IBOutlet weak var cardFour: UILabel!
+    
+    @IBAction func shuffleTouched(_ sender: Any) {
+        print("Welcome to the Cow Farm")
+        var randomCard0 = arc4random()
+        print(randomCard0 % 10)
+        if randomCard0 % 10 == 0 {
+            randomCard0 = arc4random()
+        } else {
+            cardOne.text = "\(randomCard0 % 10)"
+        }
+      
+        let randomCard1 = arc4random()
+        cardTwo.text = "\(randomCard1 % 10)"
+        
+        let randomCard2 = arc4random()
+        cardThree.text = "\(randomCard2 % 10)"
+        
+        let randomCard3 = arc4random()
+        cardFour.text = "\(randomCard3 % 10)"
+        
+    }
     
     
-   
     
 }
 
