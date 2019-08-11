e92b713678d430ee302a37b85b7ae5babc1cf128
daobao <lucashongyuanzhang@gmail.com>
2019-08-11 09:47:08 +0800

    we added GoldbachThinkTests.swift

---
+++ TwentyFourTests/GoldbachThinkTests.swift
@@ -0,0 +1,31 @@
+import XCTest
+
+class GoldbachThinkTests: XCTestCase {
+
+    func testGoldbachThink() {
+        let number = 288
+        
+        // 28 = p1 + p2 (= p1 + p1)
+        // p1, p2: 3, 285
+        // p12, p2: 5, 283, should we stop here?
+        
+        
+        if isPrime(x: 283) {
+            
+        } else {
+            
+        }
+    }
+    
+    func isPrime(x: Int) -> Bool {
+        if x % 2 == 0 {
+           return false
+        } /*else if {
+         
+        } */
+        
+        
+        
+        return false
+    }
+}
