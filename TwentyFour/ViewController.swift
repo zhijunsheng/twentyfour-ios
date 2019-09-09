359f4299adec714fcf7d5414dfced879048b66e9
Grace Huang <gracehuang@Sonics-MacBook-Air.local>
2019-09-08 19:38:43 -0500

    fix button function

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -5,29 +5,46 @@
 //  Created by Bowen Lin on 2018-05-08.
 //  Copyright © 2018 Bowen Lin. All rights reserved.
 //
-
 import UIKit
-
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var cardOneLabel: UILabel!
+    @IBOutlet weak var cardTwoLabel: UILabel!
+    @IBOutlet weak var cardThreeLabel: UILabel!
+    @IBOutlet weak var cardFourLabel: UILabel!
     
     
     @IBAction func nextHand(_ sender: UIButton) {
-        print("Hello")
         let cardOneRandom = arc4random()
         print(cardOneRandom)
         let carOneRandonSmaller = cardOneRandom % 10
         print(carOneRandonSmaller)
-        
-        // 1798154826 / 100， what is the remainder?
-        // 1798154826 % 100 => 26
-        
-        
+     
+       
         
         cardOneLabel.text = "\(carOneRandonSmaller)"
+        
+        let cardTwoRandom = arc4random()
+        print(cardTwoRandom)
+        let cardTwoRandomSmaller = cardTwoRandom % 10
+        print(cardTwoRandomSmaller)
+        
+        cardTwoLabel.text = "\(cardTwoRandomSmaller)"
+        
+        let cardThreeRandom = arc4random()
+        print(cardThreeRandom)
+        let cardThreeRandomSmaller = cardThreeRandom % 10
+        print(cardThreeRandomSmaller)
+        cardThreeLabel.text = "\(cardThreeRandomSmaller)"
+        
+        let cardFourRandom = arc4random()
+        print(cardFourRandom)
+        let cardFourRandomSmaller = cardFourRandom % 10
+        print(cardFourRandomSmaller)
+        cardFourLabel.text = "\(cardFourRandomSmaller)"
+        
+       
+        
     }
     
     
