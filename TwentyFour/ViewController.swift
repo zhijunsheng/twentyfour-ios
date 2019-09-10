40a52a6667e9b969493874f32601e2f66cd3c1c0
daobao <lucashongyuanzhang@gmail.com>
2019-09-10 19:09:52 +0800

    we added a text field and switch

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  TwentyFour
-//
-//  Created by Donald Sheng on 2018-07-24.
-//  Copyright © 2018 Gold Thumb Inc. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
