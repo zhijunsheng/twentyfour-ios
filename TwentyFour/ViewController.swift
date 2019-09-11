370ad7bc5f1070cb7540840d46deff07992d3d17
daobao <lucashongyuanzhang@gmail.com>
2019-09-11 19:57:08 +0800

    we have done of twentyfour-ios (1)

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -7,6 +7,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var cardTwo: UILabel!
     @IBOutlet weak var cardThree: UILabel!
     @IBOutlet weak var cardFour: UILabel!
+    @IBOutlet weak var dealButton: UIButton!
+    @IBOutlet weak var checkSwitch: UISwitch!
     
     var soundPlayer: AVAudioPlayer!
     
@@ -14,9 +16,17 @@ class ViewController: UIViewController {
         let soundPath = Bundle.main.path(forResource: "open_game", ofType: "wav")
         let url = URL(fileURLWithPath: soundPath!)
         soundPlayer = try? AVAudioPlayer(contentsOf: url)
+        dealButton.isEnabled = false
+        checkSwitch.isOn = false
     }
     
-    @IBAction func buttonTapped(_ sender: Any) {
+    @IBAction func checkMath(_ sender: UISwitch) {
+        if sender.isOn {
+            dealButton.isEnabled = true
+        }
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
         let numberOne = arc4random() % 10 + 1
         cardOne.text = "\(numberOne)"
         let numberTwo = arc4random()  % 10 + 1
@@ -26,16 +36,9 @@ class ViewController: UIViewController {
         let numberFour = arc4random()  % 10 + 1
         cardFour .text = "\(numberFour)"
         
-        
         soundPlayer.play()
         
-        
-        
+        checkSwitch.isOn = false
+        dealButton.isEnabled = false
     }
-
-    
 }
-
-
-
-
