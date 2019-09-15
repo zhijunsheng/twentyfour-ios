3f9a47dc41e9ec132b11c0d6b758cc0b96073128
GAMINGCEHATKILL503 <harleychan2007@gmail.com>
2019-09-15 19:40:35 -0400

    we fixed the missing 10 and simplified my code.

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -15,19 +15,46 @@ class ViewController: UIViewController { // 7 9 7 4
     @IBOutlet weak var cardThreeLabel: UILabel!
     @IBOutlet weak var cardFourLabel: UILabel!
     
-   
+    
     @IBAction func nextTapped(_ sender: UIButton) {
         
         // 48498 % 1000 = ?
-        let cardOneNumber = arc4random() % 10
-        let cardTwoNumber = arc4random() % 10
-        let cardThreeNumber = arc4random() % 10
-        let cardFourNumber = arc4random() % 10
+        let cardOneNumber = arc4random() % 10 + 1
+        let cardTwoNumber = arc4random() % 10 + 1
+        let cardThreeNumber = arc4random() % 10 + 1
+        let cardFourNumber = arc4random() % 10 + 1
         
         cardOneLabel.text = "\(cardOneNumber)"
         cardTwoLabel.text = "\(cardTwoNumber)"
         cardThreeLabel.text = "\(cardThreeNumber)"
         cardFourLabel.text = "\(cardFourNumber)"
+        
+//        for _ in 0 ... 11 {
+//            cardOneLabel.text = "\(cardOneNumber)"
+//            cardTwoLabel.text = "\(cardTwoNumber)"
+//            cardThreeLabel.text = "\(cardThreeNumber)"
+//            cardFourLabel.text = "\(cardFourNumber)"
+//            if cardOneNumber == UInt32(0) {
+//                let cardOneNumber = 1
+//                cardOneLabel.text = "\(cardOneNumber)"
+//            }
+//            if cardTwoNumber == UInt32(0) {
+//                let cardTwoNumber = 2
+//                cardTwoLabel.text = "\(cardTwoNumber)"
+//            }
+//            if cardThreeNumber == UInt32(0) {
+//                let cardThreeNumber = 3
+//                cardThreeLabel.text = "\(cardThreeNumber)"
+//                
+//            }
+//            if cardFourNumber == UInt32(0) {
+//                let cardFourNumber = 4
+//                cardFourLabel.text = "\(cardFourNumber)"
+//                
+//            }
+//            
+//            
+//        }
     }
     
 }
