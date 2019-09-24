5fba7e3162562c4fce6fa4976ea5ada8b75e735d
Elaine <elainexu392@gmail.com>
2019-09-24 19:12:23 -0400

    Made the label and the click moi have constraints

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,10 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var cardOneLabel: UILabel!
+    
+    @IBOutlet var cardTwoLabel: UILabel!
+    
+    @IBOutlet var cardThreeLabel: UILabel!
+    
+    @IBOutlet var cardFourLabel: UILabel!
     
     
-   
+    @IBAction func burttonTapped(_ sender: UIButton) {
+        let number1 = arc4random()
+        let number2 = arc4random()
+        let number3 = arc4random()
+        let number4 = arc4random()
+        
+        let numberSmall1 = number1 % 10 + 1
+        let numberSmall2 = number2 % 10 + 1
+        let numberSmall3 = number3 % 10 + 1
+        let numberSmall4 = number4 % 10 + 1
+        
+        cardOneLabel.text = "\(numberSmall1 )"
+        cardTwoLabel.text = "\(numberSmall2)"
+        cardThreeLabel.text = "\(numberSmall3 )"
+        cardFourLabel.text = "\(numberSmall4)"
+          print("hsuahciewjahfajkfha")
+    }
     
+    
+    
+  
 }
 
 
