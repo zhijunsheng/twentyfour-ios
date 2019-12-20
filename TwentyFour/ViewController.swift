12edf57653bb0419b1bae1cfe665d8411458d276
Andy Yan <andy@Andys-MacBook-Air.local>
2019-12-20 16:31:24 -0500

    we made it so that the starting hand is random

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -14,11 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondCardLabel: UILabel!
     @IBOutlet weak var thirdCardLabel: UILabel!
     @IBOutlet weak var fourthCardLabel: UILabel!
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 3, "3" (3 + 2) = 3 + 2 what happened
+        firstCardLabel.text = "\(arc4random())"
+        let b = arc4random()
+        let smallerB = b % 10 + 1
+        firstCardLabel.text = "\(smallerB)"
+        
+        secondCardLabel.text = "\(arc4random())"
+        let c = arc4random()
+        let smallerC = c % 10 + 1
+        secondCardLabel.text = "\(smallerC)"
+        
+        thirdCardLabel.text = "\(arc4random())"
+        let d = arc4random()
+        let smallerD = d % 10 + 1
+        thirdCardLabel.text = "\(smallerD)"
+        
+        fourthCardLabel.text = "\(arc4random())"
+        let e = arc4random()
+        let smallerE = e % 10 + 1
+        fourthCardLabel.text = "\(smallerE)"
+        
+        
+        
+        
+        
+    }
+    
+    
+    
     @IBAction func buttonTouched(_ sender: Any) {
         let x = arc4random()
         let smallerX = x % 10 + 1
-        print("\(smallerX)")
         firstCardLabel.text = "\(smallerX)"
         
         let y = arc4random()
