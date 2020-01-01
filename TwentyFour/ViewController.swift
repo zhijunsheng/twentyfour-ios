756f6de206c16fde0284f496142df1c75416f008
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-01-01 21:31:12 +0800

    We made the numbers saved

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -13,13 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var cardOneLabel: UILabel!
     @IBOutlet weak var cardTwoLabel: UILabel!
     @IBOutlet weak var cardThreeLabel: UILabel!
-    
     @IBOutlet weak var cardFourLabel: UILabel!
+    
+    @IBOutlet weak var card2OneLabel: UILabel!
+    @IBOutlet weak var card2TwoLabel: UILabel!
+    @IBOutlet weak var card2ThreeLabel: UILabel!
+    @IBOutlet weak var card2FourLable: UILabel!
+    
     
     @IBAction func deal(_ sender: Any) {
         
         let cardOneNum = arc4random() % 10 + 1
-        cardOneLabel.text = "\(cardOneNum)"
+        cardOneLabel.text = "\(cardOneNum)" 
         
         let cardTwoNum = arc4random() % 10 + 1
         cardTwoLabel.text = "\(cardTwoNum)"
@@ -29,5 +34,13 @@ class ViewController: UIViewController {
         
         let cardFourNum = arc4random() % 10 + 1
         cardFourLabel.text = "\(cardFourNum)"
+
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        card2OneLabel.text = "\(cardTwoLabel.text!)"
+        card2TwoLabel.text = "\(cardOneLabel.text!)"
+        card2ThreeLabel.text = "\(cardThreeLabel.text!)"
+        card2FourLable.text = "\(cardFourLabel.text!)"
     }
 }
