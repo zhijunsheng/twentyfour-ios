0dabc04ab4bf760f15a9c9412adc67e8b95c24bd
ethan <ethanyiyaozhang@gmail.com>
2020-01-02 20:02:04 +0800

    We finished 24 game.

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -13,7 +13,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func change(_ sender: Any) {
-        let a = arc4random() % 11 
+        let a = arc4random() % 10 + 1
+        let b = arc4random() % 10 + 1
+        let c = arc4random() % 10 + 1
+        let d = arc4random() % 10 + 1
         card1Label.text = "\(a)"
+        card2Label.text = "\(b)"
+        card3Label.text = "\(c)"
+        card4Label.text = "\(d)"
     }
 }
