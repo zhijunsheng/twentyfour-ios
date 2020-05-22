bd7b5e0e32537a7a845d3a39bcd07b3e55f177f7
Felix Lo <felixthegreat@Felixs-MacBook-Pro.local>
2020-05-22 11:51:11 -0400

    Added 3 cards

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -11,16 +11,33 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var card1Label: UILabel!
+    @IBOutlet weak var card2Label: UILabel!
+    @IBOutlet weak var card3Label: UILabel!
+    @IBOutlet weak var card4Label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let card1Raw = arc4random() % 10 + 1
+        let card2Raw = arc4random() % 10 + 1
+        let card3Raw = arc4random() % 10 + 1
+        let card4Raw = arc4random() % 10 + 1
+        card1Label.text = "\(card1Raw)"
+        card2Label.text = "\(card2Raw)"
+        card3Label.text = "\(card3Raw)"
+        card4Label.text = "\(card4Raw)"
     }
+    
     
     @IBAction func pressButton(_ sender: Any) {
         let card1Raw = arc4random() % 10 + 1
-        
+        let card2Raw = arc4random() % 10 + 1
+        let card3Raw = arc4random() % 10 + 1
+        let card4Raw = arc4random() % 10 + 1
         card1Label.text = "\(card1Raw)"
+        card2Label.text = "\(card2Raw)"
+        card3Label.text = "\(card3Raw)"
+        card4Label.text = "\(card4Raw)"
     }
     
     
