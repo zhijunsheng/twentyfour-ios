3c880b07f4218cf1ca0d8a91781a027eb589c57c
Grace Huang <gracehuang@Graces-MacBook-Air.local>
2021-03-14 20:01:57 -0500

    added stack

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -7,12 +7,21 @@
 //
 import UIKit
 class ViewController: UIViewController {
-    
+    var s: Stack = Stack()
     @IBOutlet weak var cardOneLabel: UILabel!
     @IBOutlet weak var cardTwoLabel: UILabel!
     @IBOutlet weak var cardThreeLabel: UILabel!
     @IBOutlet weak var cardFourLabel: UILabel!
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        s.push(n: 7)
+        s.push(n: 8)
+        print("\(s.pop()) \(s.pop())")
+        s.push(n: 2)
+        s.push(n: 3)
+        s.push(n: 4)
+        print("\(s.pop()) \(s.pop()) \(s.pop())")
+    }
     @IBAction func nextHand(_ sender: UIButton) {
         let cardOneRandom = arc4random()
         let carOneRandonSmaller = cardOneRandom % 10 + 1
@@ -30,4 +39,7 @@ class ViewController: UIViewController {
         let cardFourRandomSmaller = cardFourRandom % 10 + 1
         cardFourLabel.text = "\(cardFourRandomSmaller)"
     }
+    
+    
+    
 }
--- TwentyFourTests/FunctionTests.swift
+++ TwentyFourTests/FunctionTests.swift
@@ -56,5 +56,65 @@ class FunctionTests: XCTestCase {
         XCTAssertEqual(5, difference)
     }
     
+    func times(a: Int, b: Int) -> Int {
+        let product = a * b
+        return product
+        
+    }
+    
+    func testProduct() {
+        let product = times(a: 10, b: 5)
+        XCTAssertEqual(50, product)
+    }
+    
+    func divide(a: Int, b: Int) -> Int {
+        let quotiant = a / b
+        return quotiant
+        
+    }
+    
+    func testDivide() {
+        XCTAssertEqual(2, divide(a: 8, b: 3))
+        XCTAssertEqual(2, divide(a: 9, b: 4))
+        XCTAssertEqual(0, divide(a: 7, b: 10))
+    }
 
+    func testForLoop() {
+        for k in 0..<7 {
+            print("k = \(k), \(k * 5 + 10 )")
+        }
+    }
+    
+    /*
+ 
+     0
+     0 + 1 = 1
+     
+     
+    */
+    func testSumUp() {
+        var sum = 0
+        for x in 1...100 {
+            sum = sum + x
+        }
+        XCTAssertEqual(5050, sum)
+    }
+
+    /*
+ 
+     5!
+     1
+     1 * 2 = 2
+     2 * 3 = 6
+     6 * 4 = 24
+     24 * 5 = 120
+     
+    */
+    func testFactorial() {
+        var factorial = 1
+        for y in 1...5 {
+            factorial = factorial * y
+        }
+        XCTAssertEqual(120, factorial)
+    }
 }
