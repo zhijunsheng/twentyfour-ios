3be7e4ce1f0c4866f15c495920a70eb8b18eeb07
吴淑雅 <wushuya@miyabis-MacBook-Air.local>
2021-03-16 17:31:20 -0400

    We put four cards on the screen and a button for the next cards.

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var card1: UILabel!
+    @IBOutlet var card2: UILabel!
+    @IBOutlet var card3: UILabel!
+    @IBOutlet var card4: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func nextCards(_ sender: Any) {
+        let random1 = arc4random()
+        card1.text = "\(random1 % 10)"
+        
+        // code for card2
+        let random2 = arc4random()
+        card2.text = "\(random2 % 10) "
+        // code for card3
+        let random3 = arc4random()
+        card3.text = "\(random3 % 10)"
+        let random4 = arc4random()
+        card4.text = "\(random4 % 10)"
+        
+        // 7 / 2 r
+        // 17 / 5
+        // 17 / 10 r 7
+        // 23 / 10 r 3
+        // 120 / 20 r 0
+        // 120 / 10 r 0
+        // 120 / 100 r 20
+        // 23341120 / 100 r 20
+    }
+    
 }
 
-
-
+   
+// (9 - 2) * 4 - 4 = 24
 
