70fe7a16847655fdf60f2c34d0b7510532a98e11
Harry Shen <onetrick@Harrys-MBP.fios-router.home>
2021-05-22 18:24:13 -0400

    Finished Game

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -9,10 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+        
+    @IBOutlet weak var cardOneLabel: UILabel!
+    @IBOutlet weak var cardTwoLabel: UILabel!
+    @IBOutlet weak var cardThreeLabel: UILabel!
+    @IBOutlet weak var cardFourLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Hello")
+        cardOneLabel.text = "10"
+        
     }
+    
+    @IBAction func giveNextHand(_ sender: Any) {
+        print("Hello1")
+        let cardOne = arc4random() % 10 + 1
+        let cardTwo = arc4random() % 10 + 1
+        let cardThree = arc4random() % 10 + 1
+        let cardFour = arc4random() % 10 + 1
+        cardOneLabel.text = "\(cardOne)"
+        cardTwoLabel.text = "\(cardTwo)"
+        cardThreeLabel.text = "\(cardThree)"
+        cardFourLabel.text = "\(cardFour)"
+        
+    }
+    
 }
 
 
