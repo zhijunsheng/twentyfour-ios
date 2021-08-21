9974a3471c45d53a45892ee7d1f8ee04012f404c
Lambert Lin <lambertsixianlin@gmail.com>
2021-08-21 21:45:25 +0800

    We made the code which could read the postfix expressions.

--- TwentyFourTests/StackTests.swift
+++ TwentyFourTests/StackTests.swift
@@ -10,6 +10,57 @@ import XCTest
 @testable import TwentyFour
 
 class StackTests: XCTestCase {
+    
+    // "2 3 +" => 5
+    /*
+     
+     
+     
+     */
+    // "9 7 - 1 + 8 * " => 24
+    /*
+     FILO stack
+     ---------
+     |24
+     ---------
+     7 9 -
+     1 2 +
+     8 3 *
+     ...
+     */
+    func parse(expr: String) -> Int {
+        let arr = expr.components(separatedBy: " ")
+        print(arr)
+        var stack: Stack = Stack()
+        
+        for sym in arr {
+            print("\(sym)") // "7"
+            if sym == "+" {
+                let a = stack.pop()
+                let b = stack.pop()
+                let sum = a + b
+                stack.push(a: sum)
+            } else if sym == "-" {
+                
+                
+            } else {
+                stack.push(a: Int(sym)!)
+            }
+        }
+        let answer = stack.pop()
+        
+        
+        return answer
+    }
+    
+    func testParse() {
+//        XCTAssertEqual(24, parse(expr: "9 7 - 1 + 8 * "))
+//        XCTAssertEqual(5, parse(expr: "2 3 +"))
+//        XCTAssertEqual(12, parse(expr: "2 3 7 + +"))
+        XCTAssertEqual(12, parse(expr: "2 3 + 7 +"))
+        
+    }
+    
     func testPush() {
         var stack: Stack = Stack()
         stack.push(a: 4)
