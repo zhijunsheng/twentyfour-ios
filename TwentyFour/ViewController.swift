788b87cc5c07d1cb126cbb0cae57664fabb8b11e
Elaine Xu <elainexu392@gmail.com>
2021-08-24 19:50:32 -0400

    Made the starting numbers random.

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -19,9 +19,9 @@ class ViewController: UIViewController {
     
     @IBOutlet var cardFourLabel: UILabel!
     
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        
-        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
         let number1 = arc4random()
         let number2 = arc4random()
         let number3 = arc4random()
@@ -36,7 +36,24 @@ class ViewController: UIViewController {
         cardTwoLabel.text = "\(numberSmall2)"
         cardThreeLabel.text = "\(numberSmall3 )"
         cardFourLabel.text = "\(numberSmall4)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let number1 = arc4random()
+        let number2 = arc4random()
+        let number3 = arc4random()
+        let number4 = arc4random()
+
+        let numberSmall1 = number1 % 10 + 1
+        let numberSmall2 = number2 % 10 + 1
+        let numberSmall3 = number3 % 10 + 1
+        let numberSmall4 = number4 % 10 + 1
         
+        cardOneLabel.text = "\(numberSmall1 )"
+        cardTwoLabel.text = "\(numberSmall2)"
+        cardThreeLabel.text = "\(numberSmall3 )"
+        cardFourLabel.text = "\(numberSmall4)"
+
         textBox.text = ""
     }
     
