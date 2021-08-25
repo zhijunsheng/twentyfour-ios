46c35aa3827976bab9f077c36d3392cf6595107a
Lambert Lin <lambertsixianlin@gmail.com>
2021-08-25 21:59:53 +0800

    We made the parse half successful.

--- TwentyFourTests/StackTests.swift
+++ TwentyFourTests/StackTests.swift
@@ -28,37 +28,39 @@ class StackTests: XCTestCase {
      8 3 *
      ...
      */
-    func parse(expr: String) -> Int {
-        let arr = expr.components(separatedBy: " ")
+    func parse(expr: String) -> Int { // expr = "9 7 -"
+        let arr = expr.components(separatedBy: " ") // ["9", "7", "-"]
         print(arr)
-        var stack: Stack = Stack()
+        var stack: Stack = Stack() // we create a stack for use
         
         for sym in arr {
-            print("\(sym)") // "7"
+            print("\(sym)")
             if sym == "+" {
                 let a = stack.pop()
                 let b = stack.pop()
                 let sum = a + b
                 stack.push(a: sum)
             } else if sym == "-" {
-                
-                
+                let num1 = stack.pop()
+                let num2 = stack.pop()
+                stack.push(a: num2 - num1)
             } else {
                 stack.push(a: Int(sym)!)
             }
         }
+        
         let answer = stack.pop()
-        
-        
         return answer
     }
     
     func testParse() {
 //        XCTAssertEqual(24, parse(expr: "9 7 - 1 + 8 * "))
-//        XCTAssertEqual(5, parse(expr: "2 3 +"))
-//        XCTAssertEqual(12, parse(expr: "2 3 7 + +"))
+        XCTAssertEqual(5, parse(expr: "2 3 +"))
+        XCTAssertEqual(12, parse(expr: "2 3 7 + +"))
         XCTAssertEqual(12, parse(expr: "2 3 + 7 +"))
-        
+        XCTAssertEqual(2, parse(expr: "9 7 -"))
+        XCTAssertEqual(14, parse(expr: "13 4 5 - -"))
+        XCTAssertEqual(12, parse(expr: "13 4 + 5 -"))
     }
     
     func testPush() {
