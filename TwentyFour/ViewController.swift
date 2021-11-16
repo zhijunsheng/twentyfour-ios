364bea3bc3298046dac360854f149259afe6bbd3
Kenneth Wu <kennethwu@Sophias-MacBook-Pro.local>
2021-11-16 16:14:38 -0500

    We improved our 24 game and made the starting hand more playable.

--- TwentyFour/ViewController.swift
+++ TwentyFour/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var card1: UILabel!
+    @IBOutlet weak var card2: UILabel!
+    @IBOutlet weak var card3: UILabel!
+    @IBOutlet weak var card4: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        randomCards()
+        
     }
+    
+    @IBAction func nextHand(_ sender: Any) {
+
+        randomCards()
+    }
+    
+    func randomCards() {
+        let random = arc4random() % 10 + 1
+        let random2 = arc4random() % 10 + 1
+        let random3 = arc4random() % 10 + 1
+        let random4 = arc4random() % 10 + 1
+        
+        card1.text = "\(random)"
+        card2.text = "\(random2)"
+        card3.text = "\(random3)"
+        card4.text = "\(random4)"
+    }
+    
 }
 
 
